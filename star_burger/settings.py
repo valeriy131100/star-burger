fe00ea715c951fe8c07b50c30aa67e6f4c6d82b1
Valery Efremov <valeriy131100@gmail.com>
2022-03-24 09:50:16 +0300

    Exclude 404 from Rollbar

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware'
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddlewareExcluding404'
 ]
 
 ROOT_URLCONF = 'star_burger.urls'
